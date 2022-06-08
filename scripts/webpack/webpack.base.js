3a5abfeaf695c0b18f7dd082722333c13e5b7eaa
ziqiang_wang <1933608667@qq.com>
2022-06-08 16:51:39 +0800

    fix：去除没有用到的资源包

--- scripts/webpack/webpack.base.js
+++ scripts/webpack/webpack.base.js
@@ -1,18 +1,21 @@
-const path = require('path');
-const { dirs, modules, entries } = require('./base');
+const path = require("path");
+const { dirs, modules, entries } = require("./base");
 
 module.exports = {
   context: dirs.src,
-
   entry: entries,
-
   resolve: {
-    modules: [dirs.src, dirs.modules],
-    extensions: ['.js', '.ts', '.tsx'],
+    extensions: [".js", ".ts", ".tsx"],
     alias: {
-      '~': dirs.src,
-      '@assets': path.resolve(dirs.src, './assets')
-    }
+      "~": dirs.src,
+      "@assets": path.resolve(dirs.src, "./assets"),
+    },
   },
-  module: modules
+  module: modules,
+  //规定一个性能指标，当达到该指标提示报错。文档地址：https://webpack.docschina.org/configuration/performance/#performance
+  performance: {
+    hints: "error", //报错提示
+    maxAssetSize: 1024 * 1024 * 10, //此选项根据单个资源体积
+    maxEntrypointSize: 1024 * 1024 * 10, //此选项根据入口起点的最大体积
+  },
 };
